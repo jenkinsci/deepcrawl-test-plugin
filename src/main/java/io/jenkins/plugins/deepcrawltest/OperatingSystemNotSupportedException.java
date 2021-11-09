162dabed5d91486227131119bd4f42fd60a7a869
Andrei Stefan Bitca <andrei.bitca@deepcrawl.com>
2021-11-09 11:49:29 +0000

    refactor: remove unused exception

--- src/main/java/io/jenkins/plugins/deepcrawltest/OperatingSystemNotSupportedException.java
+++
@@ -1,7 +0,0 @@
-package io.jenkins.plugins.deepcrawltest;
-
-public class OperatingSystemNotSupportedException extends java.io.IOException { 
-  public OperatingSystemNotSupportedException() {
-    super("Operating system is not supported!");
-  }
- }
